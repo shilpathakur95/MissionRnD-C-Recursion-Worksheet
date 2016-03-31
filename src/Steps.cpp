c530d95d072e99743a18197b637be97593d7e690
shilpathakur95 <shilpathakur95@missionrnd.com>
2016-03-31 20:57:05 +0530

    Submit Commit

--- src/Steps.cpp
+++ src/Steps.cpp
@@ -24,7 +24,46 @@
 #include "stdafx.h"
 
 
+int fact(int num)
+{
+	int c, facto = 1;
+	for (c = 1; c <= num; c++)
+		facto = facto * c;
+	return facto;
+}
+int rec(int twos, int ones)
+{
+	int temp;
+	if (twos > 1)
+	{
+		ones += 2;
+		twos--;
+		temp = fact(ones + twos) / (fact(ones)*fact(twos));
+	}
+	else
+		return 0;
+	return temp + rec(twos, ones);
+}
 int get_steps(int s)
 {
-	return 0;
+	if (s == 0)
+		return 0;
+	if (s == 1)
+		return 1;
+	int count = 1, temp, ones = 0, twos = 0;
+	if (s % 2 == 0)
+	{
+		count++;
+		twos = s / 2;
+		count += rec(twos, ones);
+	}
+	else
+	{
+		twos = (s - 1) / 2;
+		ones = 1;
+		temp = fact(ones + twos) / (fact(ones)*fact(twos));
+		count = count + temp;
+		count += rec(twos, ones);
+	}
+	return count;
 }
